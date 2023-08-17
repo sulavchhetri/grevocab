0689667058e747c188e360e17116f84c690e4b70
SulavChhetri <sulavthapachhetri@gmail.com>
2023-08-18 00:51:28 +0545

    minor fix

--- grevocab/vocabulary.py
+++ grevocab/vocabulary.py
@@ -30,6 +30,8 @@ def get_more_info(tag):
 def get_definitions(soup):
     result = []
     tag = soup.find("div", {"class": "word-definitions"})
+    if not tag:
+        return result
     words = tag.find_all("li")
     if not words:
         return result
