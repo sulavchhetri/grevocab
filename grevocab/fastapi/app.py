c8e9ffd67af81cbcf7ef648d952b6c723a3dd4ae
SulavChhetri <sulavthapachhetri@gmail.com>
2023-08-18 11:23:50 +0545

    latest changes

--- grevocab/fastapi/app.py
+++ grevocab/fastapi/app.py
@@ -10,6 +10,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.templating import Jinja2Templates
 from grevocab.fastapi.util import PrettyJSONResponse
 from grevocab.main import scrape_words
+from starlette.responses import HTMLResponse
 
 gre_files_path = os.path.join(
     Path(__file__).parent.parent.parent, "files", "gregmat.json")
@@ -36,8 +37,8 @@ def process_get(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
 
-@app.post("/", response_class=PrettyJSONResponse)
-def process(words: str = Form()):
+@app.post("/", response_class=HTMLResponse)
+def process(request: Request, words: str = Form()):
     """
         This function take the form data, performs a function on it and returns
         the result
@@ -45,17 +46,18 @@ def process(words: str = Form()):
     if not words:
         return {"data": None}
     words_list = words.split(',')
-    return {'data': scrape_words(words=words_list)}
+    data = scrape_words(words=words_list)
+    return templates.TemplateResponse("output.html", {"data": data, "request": request})
 
 
-@app.get("/gregmat", response_class=PrettyJSONResponse)
-def get_gregmat_words():
+@app.get("/gregmat", response_class=HTMLResponse)
+def get_gregmat_words(request: Request):
     """
         This function renders the gregmat data.
     """
     with open(gre_files_path, 'r', encoding='utf-8') as file:
         data = json.load(file)
-    return data
+    return templates.TemplateResponse("output.html", {"data": data, "request": request})
 
 
 if __name__ == "__main__":
--- grevocab/main.py
+++ grevocab/main.py
@@ -1,11 +1,41 @@
 """
     This module is used to scrape the words from the vocabulary.com
 """
+import os
+import json
 import random
 import time
+from pathlib import Path
 from loguru import logger
 from grevocab.vocabulary import get_definitions
 from grevocab.utils import get_soup
+
+all_words_path = os.path.join(Path(__file__).parent.parent,
+                              "files", "all_words.json")
+
+
+def get_all_words():
+    """
+        This function is used to get the all_words file
+    """
+    try:
+        with open(all_words_path, 'r', encoding='utf-8') as file:
+            all_words = json.load(file)
+        return all_words
+    except:
+        return {}
+
+
+def save_all_words(data):
+    """
+        This function is used to get the all_words file
+    """
+    try:
+        with open(all_words_path, 'w', encoding='utf-8') as file:
+            json.dump(data, file, ensure_ascii=False, default=str)
+    except:
+        pass
+
 
 def scrape_words(words):
     """
@@ -15,12 +45,16 @@ def scrape_words(words):
 
         Returns: list[dict] : Info about the words
     """
+    all_words = get_all_words()
     result = {}
     count = 0
     for word in words:
-        logger.info("The word is {}",word)
+        if all_words.get(word, None):
+            result[word] = all_words[word]
+            continue
+        logger.info("The word is {}", word)
         if count == 5:
-            logger.info("word {} is in count {}",word, count)
+            logger.info("word {} is in count {}", word, count)
             return result
         soup = get_soup(word)
         if not soup:
@@ -29,4 +63,6 @@ def scrape_words(words):
             continue
         result[word] = get_definitions(soup)
         time.sleep(random.uniform(1, 4))
+    all_words.update(**result)
+    save_all_words(all_words)
     return result
